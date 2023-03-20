d6e05a4cb9bb1852a841c3790483adaad5ca6d41
FelipeLL <f3lip3.3z@hotmail.com>
2023-03-19 21:50:38 -0500

    feat(helpers): add show menu

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 require('colors');
-console.clear();
+const { showMenu, pause } = require('./helpers/messages.js');
 
 const main = async () => {
+    let opt = ''
+    do {
+        opt = await showMenu();
 
+        if (opt !== '0') {
+            await pause();
+        }
+
+        if (opt === '0') {
+            console.log('\nHas salido de la aplicación\n'.magenta);
+        }
+
+    } while (opt !== '0');
 }
 
 main();
